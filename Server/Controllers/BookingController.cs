3ac846897d8327eaf2c94cfe943d800227cbefb1
jakobhaulund <jakobhaulund@hotmail.com>
2023-05-09 14:18:16 +0200

    test

---
+++ Server/Controllers/BookingController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Server.Controllers
+{
+	public class BookingController
+	{
+		public BookingController()
+		{
+		}
+	}
+}
+
---
+++ Server/Controllers/KoordinatorController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Server.Controllers
+{
+	public class KoordinatorController
+	{
+		public KoordinatorController()
+		{
+		}
+	}
+}
+
